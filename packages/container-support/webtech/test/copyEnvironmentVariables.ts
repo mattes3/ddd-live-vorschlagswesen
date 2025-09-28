ff5552ae921f56f55629005db7d9252e97bbdcae
Matthias Bohlen <mbohlen@mbohlen.de>
2025-09-28 15:05:20 +0200

    chore: Alte tests inkl. vitest entfernt...
    vitest war immer wieder Ursache von TypeScript-Compilerfehlern im Zusammenhang mit rollup. Jetzt verwende ich `node:test` und `expect`.

--- packages/container-support/webtech/test/copyEnvironmentVariables.ts
+++
@@ -1,8 +0,0 @@
-export function copyEnvironmentVariables() {
-    process.env['POSTGRES_HOSTNAME'] = process.env['POSTGRES_DEV_HOSTNAME'];
-    process.env['POSTGRES_PORT'] = process.env['POSTGRES_DEV_PORT'];
-    process.env['POSTGRES_USER'] = process.env['POSTGRES_DEV_USER'];
-    process.env['POSTGRES_PASSWORD'] = process.env['POSTGRES_DEV_PASSWORD'];
-    process.env['POSTGRES_DB'] = process.env['POSTGRES_DEV_DB'];
-    process.env['POSTGRES_SSL'] = process.env['POSTGRES_DEV_SSL'];
-}
